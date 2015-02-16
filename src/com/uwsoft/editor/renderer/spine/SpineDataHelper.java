cd59e3f8c483f03d40bcab37fc830cacb0b228ea
gevorg-kopalyan <gevorg.kopalyan@gmail.com>
2015-02-16 19:00:45 +0400

    removing reflectAsm lib using libgdx reflection utils instead

--- src/com/uwsoft/editor/renderer/spine/SpineDataHelper.java
+++ src/com/uwsoft/editor/renderer/spine/SpineDataHelper.java
@@ -2,9 +2,12 @@ package com.uwsoft.editor.renderer.spine;
 
 import java.lang.reflect.InvocationTargetException;
 
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.utils.Array;
-import com.esotericsoftware.reflectasm.MethodAccess;
+import com.badlogic.gdx.utils.reflect.ClassReflection;
+import com.badlogic.gdx.utils.reflect.Method;
+import com.badlogic.gdx.utils.reflect.ReflectionException;
 import com.uwsoft.editor.renderer.data.SpineVO;
 import com.uwsoft.editor.renderer.resources.IResourceRetriever;
 
@@ -20,52 +23,63 @@ public class SpineDataHelper {
 	public float width;
 	public float height;
 	
-	public void initSpine(SpineVO dataVO, IResourceRetriever rm, SpineReflectionHelper refData, float mulX) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void initSpine(SpineVO dataVO, IResourceRetriever rm, SpineReflectionHelper refData, float mulX) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ReflectionException {
 		
 		renderer = refData.skeletonRendererObject;
 		reflectionData = refData;
 		
 		Object skeletonJsonObject = reflectionData.skeletonJsonConstructorAccess.newInstance(rm.getSkeletonAtlas(dataVO.animationName));
 		
-		MethodAccess methodAccess = MethodAccess.get(reflectionData.skeletonJsonClass);
-		methodAccess.invoke(skeletonJsonObject, "setScale", dataVO.scaleX * mulX);
+		Method setScaleMethodAccess = ClassReflection.getMethod(reflectionData.skeletonJsonClass, "setScale", float.class);	
+		setScaleMethodAccess.invoke(skeletonJsonObject, dataVO.scaleX * mulX);
 		
-		skeletonData = methodAccess.invoke(skeletonJsonObject, "readSkeletonData", rm.getSkeletonJSON(dataVO.animationName));
+		Method readSkeletonData = ClassReflection.getMethod(reflectionData.skeletonJsonClass, "readSkeletonData", FileHandle.class);	
+		skeletonData = readSkeletonData.invoke(skeletonJsonObject, rm.getSkeletonJSON(dataVO.animationName));
 		
 		skeletonObject = reflectionData.skeletonConstructorAccess.newInstance(skeletonData);
 		
 		Object animationStateDataObject = reflectionData.animationStateDataConstructorAccess.newInstance(skeletonData);
 		
+		
 		computeBoundBox();
+		
 
 		stateObject = reflectionData.animationStateConstructorAccess.newInstance(animationStateDataObject);
 		
-		Array<Object> anims = (Array<Object>) reflectionData.skeletonDataClassMethodAccess.invoke(skeletonData, reflectionData.getAnimationMethodIndex);
+		//Array<Object> anims = (Array<Object>) reflectionData.skeletonDataClassMethodAccess.invoke(skeletonData, reflectionData.getAnimationMethodIndex);
+		Array<Object> anims = (Array<Object>) reflectionData.getAnimationMethodIndex.invoke(skeletonData);
 		
 		Object animObj = anims.get(0);
-		String animName = (String) reflectionData.animationClassMethodAccess.invoke(animObj, reflectionData.getAnimNameMethodIndex);
+		//String animName = (String) reflectionData.animationClassMethodAccess.invoke(animObj, reflectionData.getAnimNameMethodIndex);
+		String animName = (String) reflectionData.getAnimNameMethodIndex.invoke(animObj);
 		setAnimation(animName);
 	}
 
-	private void computeBoundBox() {
-		reflectionData.skeletonClassMethodAccess.invoke(skeletonObject, reflectionData.updateWorldTransformMethodIndex);
+	private void computeBoundBox() throws ReflectionException{
+		//reflectionData.skeletonClassMethodAccess.invoke(skeletonObject, reflectionData.updateWorldTransformMethodIndex);
+		reflectionData.updateWorldTransformMethodIndex.invoke(skeletonObject);
+		
 		minX = Float.MAX_VALUE;
 		minY = Float.MAX_VALUE;
 		float maxX = Float.MIN_VALUE, maxY = Float.MIN_VALUE;
 		
-		Array<Object> slots = (Array<Object>) reflectionData.skeletonClassMethodAccess.invoke(skeletonObject, reflectionData.getSlotsMethodIndex);
+		//Array<Object> slots = (Array<Object>) reflectionData.skeletonClassMethodAccess.invoke(skeletonObject, reflectionData.getSlotsMethodIndex);
+		Array<Object> slots = (Array<Object>) reflectionData.getSlotsMethodIndex.invoke(skeletonObject);
 		
 		for (int i = 0, n = slots.size; i < n; i++) {
 			Object slot = slots.get(i);
-			Object attachment = reflectionData.slotClassMethodAccess.invoke(slot, reflectionData.getAttachmentMethodIndex);
+			//Object attachment = reflectionData.slotClassMethodAccess.invoke(slot, reflectionData.getAttachmentMethodIndex);
+			Object attachment = reflectionData.getAttachmentMethodIndex.invoke(slot);
 			if (attachment == null)
 				continue;
 			if (!(reflectionData.regionAttachmentClass.isInstance(attachment)))
 				continue;
 
-			reflectionData.regionAttachmentMethodAccess.invoke(attachment, reflectionData.updateWorldVerticesMethodIndex,slot,false);
+			//reflectionData.regionAttachmentMethodAccess.invoke(attachment, reflectionData.updateWorldVerticesMethodIndex,slot,false);
+			reflectionData.updateWorldVerticesMethodIndex.invoke(attachment, slot, false);
 			
-			float[] vertices = (float[]) reflectionData.regionAttachmentMethodAccess.invoke(attachment, reflectionData.getWorldVerticesIndex);
+			//float[] vertices = (float[]) reflectionData.regionAttachmentMethodAccess.invoke(attachment, reflectionData.getWorldVerticesIndex);
+			float[] vertices = (float[]) reflectionData.getWorldVerticesIndex.invoke(attachment);
 			for (int ii = 0, nn = vertices.length; ii < nn; ii += 5) {
 				minX = Math.min(minX, vertices[ii]);
 				minY = Math.min(minY, vertices[ii + 1]);
@@ -83,20 +97,38 @@ public class SpineDataHelper {
 			System.out.println("NO ANIM NAME");
 			return;
 		}
-		reflectionData.stateObjectMethodAccess.invoke(stateObject,reflectionData.setAnimationMethodIndex,0, animName, true);
+		//reflectionData.stateObjectMethodAccess.invoke(stateObject,reflectionData.setAnimationMethodIndex,0, animName, true);
+		try {
+			reflectionData.setAnimationMethodIndex.invoke(stateObject,0, animName, true);
+		} catch (ReflectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public Array<Object> getAnimations(){
-		return (Array<Object>) reflectionData.skeletonDataClassMethodAccess.invoke(skeletonData, reflectionData.getAnimationMethodIndex);
+		try {
+			return (Array<Object>) reflectionData.getAnimationMethodIndex.invoke(skeletonData);
+		} catch (ReflectionException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public void draw(Batch batch, float parentAlpha) {
-		reflectionData.skeletonRendererMethodAccess.invoke(renderer, reflectionData.skeletonRendererDrawMethodIndex, batch, skeletonObject);
+		//reflectionData.skeletonRendererMethodAccess.invoke(renderer, reflectionData.skeletonRendererDrawMethodIndex, batch, skeletonObject);
+		try {
+			reflectionData.skeletonRendererDrawMethodIndex.invoke(renderer, batch, skeletonObject);
+		} catch (ReflectionException e) {
+		}
 	}
 
 	public void act(float delta, float x, float y) {
-		reflectionData.skeletonClassMethodAccess.invoke(skeletonObject, reflectionData.updateWorldTransformMethodIndex);
-		reflectionData.stateObjectMethodAccess.invoke(stateObject,reflectionData.updateMethodIndex,delta);
-		reflectionData.stateObjectMethodAccess.invoke(stateObject,reflectionData.applyMethodIndex,skeletonObject);
-		reflectionData.skeletonClassMethodAccess.invoke(skeletonObject,reflectionData.setPositionMethodIndex,x - minX, y - minY);
+		try {
+			reflectionData.updateWorldTransformMethodIndex.invoke(skeletonObject);
+			reflectionData.updateMethodIndex.invoke(stateObject, delta);
+			reflectionData.applyMethodIndex.invoke(stateObject, skeletonObject);
+			reflectionData.setPositionMethodIndex.invoke(skeletonObject, x - minX, y - minY);
+		}catch (ReflectionException e) {
+		}
 	}
 }
