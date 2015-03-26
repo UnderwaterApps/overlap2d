80fa30fd291074e4ee2f157585f21eb46cef78ad
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2015-03-26 17:22:46 +0400

    javadoc comments progress

--- src/com/uwsoft/editor/controlles/flow/FlowActionEnum.java
+++ src/com/uwsoft/editor/controlles/flow/FlowActionEnum.java
@@ -1,10 +1,24 @@
 package com.uwsoft.editor.controlles.flow;
 
 /**
+ * Simple list of possible types of user actions that are "Undoable"
+ *
  * Created by sargis on 7/23/14.
  */
 public enum FlowActionEnum {
+	 /**
+	  * SIMPLE is any simple user action that does not include getting in or out of composites
+	  * TODO: this is supposed to be devided by more types in future, instead of keeping it generic and simple
+	  */
     SIMPLE,
+
+	 /**
+	  * GET_INTO_COMPOSITE - user does no modification but enters into composite item
+	  */
     GET_INTO_COMPOSITE,
+
+	 /**
+	  * GET_OUT_COMPOSITE - user does no modification but exists currently opened composite item
+	  */
     GET_OUT_COMPOSITE
 }
--- src/com/uwsoft/editor/controlles/flow/HistoryData.java
+++ src/com/uwsoft/editor/controlles/flow/HistoryData.java
@@ -3,9 +3,18 @@ package com.uwsoft.editor.controlles.flow;
 import com.uwsoft.editor.renderer.data.CompositeItemVO;
 
 /**
+ * This is a history data point created on each user action.
+ *
  * Created by sargis on 7/23/14.
  */
 class HistoryData {
+	 /**
+	  * The snapshot of scene, stored as a VO data object of root element of the scene.
+	  */
     public CompositeItemVO compositeItemVO;
+
+	 /**
+	  * The type of user action
+	  */
     public FlowActionEnum action;
 }
