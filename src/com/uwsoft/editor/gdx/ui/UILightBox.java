b48c325e1ca7f27b527999d1f92893465074142a
Osman <dnny_os@yahoo.de>
2015-04-07 11:21:18 +0200

    - colorpicker (tint, ambience) picks respective current color initially - fixed bug: ambience color change affecting scene visually although affection is disabled

--- src/com/uwsoft/editor/gdx/ui/UILightBox.java
+++ src/com/uwsoft/editor/gdx/ui/UILightBox.java
@@ -88,9 +88,15 @@ public class UILightBox extends ExpandableUIBox {
                         cPicker.setColorValue(newColor);
 
                         // change scene ambient light
-                        stage.getSandbox().setSceneAmbientColor(newColor);
+                        stage.getSandbox().setSceneAmbientColor(newColor, !disableAmbiance.isChecked());
                     }
                 });
+
+                final float[] ambientColor = stage.getSandbox().getSceneControl().getCurrentSceneVO().ambientColor;
+                //TODO might be more wise to manage a single tmp color instead of recreating new colors
+                picker.setColor(new Color(
+                        ambientColor[0], ambientColor[1],
+                        ambientColor[2], ambientColor[3]));
                 stage.addActor(picker.fadeIn());
             }
         });
@@ -104,6 +110,7 @@ public class UILightBox extends ExpandableUIBox {
                         cPickerElems.setColorValue(newColor);
                     }
                 });
+
                 stage.addActor(picker.fadeIn());
             }
         });
