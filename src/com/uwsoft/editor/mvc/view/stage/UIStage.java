50857e63693066fd7c909039b8b8827ea1ad9e53
Sargis Sargsyan <sargsyan.sargis.89@gmail.com>
2015-04-09 14:05:42 +0400

    fixing

--- src/com/uwsoft/editor/mvc/view/stage/UIStage.java
+++ src/com/uwsoft/editor/mvc/view/stage/UIStage.java
@@ -88,10 +88,6 @@ public class UIStage extends Stage {
 
 
         mainDropDown = new DropDown(contextMenuContainer);
-
-        // not sure why this is necessary
-        // TODO: temporary fix
-        essentials.rayHandler.setAmbientLight(1, 1, 1, 1);
     }
 
     public Sandbox getSandbox() {
