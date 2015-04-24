daa6e66ef2a5ef84dffd7947a788c63a771897f1
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2015-04-24 17:05:02 +0400

    some fixes to font system, and interplatformisation

--- src/com/uwsoft/editor/mvc/view/ui/box/UIFontChooserMediator.java
+++ src/com/uwsoft/editor/mvc/view/ui/box/UIFontChooserMediator.java
@@ -1,27 +1,15 @@
 package com.uwsoft.editor.mvc.view.ui.box;
 
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.utils.Array;
 import com.puremvc.patterns.mediator.SimpleMediator;
 import com.puremvc.patterns.observer.Notification;
 
 import com.uwsoft.editor.mvc.Overlap2DFacade;
 import com.uwsoft.editor.mvc.proxy.ProjectManager;
 import com.uwsoft.editor.mvc.proxy.TextureManager;
-import org.apache.commons.io.FileUtils;
-
-import java.awt.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.uwsoft.editor.utils.FontUtils;
 
 
 /**
@@ -31,17 +19,11 @@ public class UIFontChooserMediator extends SimpleMediator<UIFontChooser> {
     private static final String TAG = UIFontChooserMediator.class.getCanonicalName();
     public static final String NAME = TAG;
 
-    private HashMap<String, String> systemFontMap = new HashMap<>();
+    FontUtils fontUtils = new FontUtils();
 
     public UIFontChooserMediator() {
         super(NAME, new UIFontChooser());
 
-        // get system fonts
-        /*
-        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-        Array<String> fontList = new Array<>(fonts);
-        viewComponent.setSelectBoxItems(fontList);*/
-        generateFontList();
     }
 
     @Override
@@ -64,6 +46,7 @@ public class UIFontChooserMediator extends SimpleMediator<UIFontChooser> {
 
         switch (notification.getName()) {
             case ProjectManager.PROJECT_OPENED:
+                generateFontList();
                 break;
             case UIFontChooser.FONT_SELECTED:
                 fontSelected(viewComponent.getSelectedFont());
@@ -75,40 +58,9 @@ public class UIFontChooserMediator extends SimpleMediator<UIFontChooser> {
 
     private void generateFontList() {
 
-        Preferences prefs = Gdx.app.getPreferences("o2d_prefs_fonts");
+        fontUtils.generateFontsMap();
 
-        systemFontMap = (HashMap<String, String>)prefs.get();
-
-        String path = System.getenv("WINDIR");
-        File fontDirectory = new File(path, "Fonts");
-        String[] extensions = new String[] { "ttf" };
-        List<File> files = (List<File>) FileUtils.listFiles(fontDirectory, extensions, true);
-        Array<String> fontList = new Array<>();
-
-        for (File file : files) {
-            Font f = null;
-            try {
-                if(!systemFontMap.containsValue(file.getAbsolutePath())) {
-                    f = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(file.getAbsolutePath()));
-                    String name = f.getName();
-                    systemFontMap.put(name, file.getAbsolutePath());
-                }
-            } catch (FontFormatException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        for (Map.Entry<String, String> entry : systemFontMap.entrySet()) {
-            fontList.add(entry.getKey());
-        }
-
-        prefs.put(systemFontMap);
-        prefs.flush();
-
-        viewComponent.setSelectBoxItems(fontList);
+        viewComponent.setSelectBoxItems(fontUtils.getFontNamesFromMap());
     }
 
     private void fontSelected(String fontName) {
@@ -116,40 +68,9 @@ public class UIFontChooserMediator extends SimpleMediator<UIFontChooser> {
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 18;
 
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(new FileHandle(systemFontMap.get(fontName)));
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(fontUtils.getTTFByName(fontName));
         BitmapFont font = generator.generateFont(parameter);
 
-
-
-        /*
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 18;
-        parameter.characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\\n1234567890\\\"!`?'.,;:()[]{}<>|/@\\\\^$-%+=#_&~*\\u007F";
-
-        String tempDir = System.getProperty("java.io.tmpdir");
-
-        UnicodeFont unicodeFont = new UnicodeFont(Font.decode(fontName), parameter.size, false, false);
-        fontName = fontName.replaceAll(" ", "");
-        unicodeFont.addGlyphs(parameter.characters);
-        ColorEffect colorEffect = new ColorEffect();
-        colorEffect.setColor(new Color(1, 1, 1, 1));
-        unicodeFont.getEffects().add(colorEffect);
-        unicodeFont.setPaddingTop(2);
-        unicodeFont.setPaddingRight(2);
-        unicodeFont.setPaddingBottom(2);
-        unicodeFont.setPaddingLeft(2);
-
-        unicodeFont.setNativeRendering(true);
-        BMFontUtil util = new BMFontUtil(unicodeFont);
-        File fontFile = new File(tempDir + "\\" + fontName);
-        try {
-            util.save(fontFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        BitmapFont font = new BitmapFont(new FileHandle(fontFile.getAbsolutePath() + ".fnt"));
-*/
         TextureManager textureManager = Overlap2DFacade.getInstance().retrieveProxy(TextureManager.NAME);
         textureManager.addBitmapFont(fontName.replaceAll(" ", ""), parameter.size, font);
 
---
+++ src/com/uwsoft/editor/utils/FontUtils.java
@@ -0,0 +1,140 @@
+package com.uwsoft.editor.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Array;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
+
+/**
+ * Created by CyberJoe on 4/24/2015.
+ */
+public class FontUtils {
+
+    private static final String cache_name = "overlap2d-fonts-cache";
+
+    private Preferences prefs;
+
+    private HashMap<String, String> systemFontMap = new HashMap<>();
+
+    public FontUtils() {
+        prefs = Gdx.app.getPreferences(cache_name);
+    }
+
+    public String[] getSystemFontNames() {
+        Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        String[] names = new String[fonts.length];
+        for(int i = 0; i < fonts.length; i++) {
+            names[i] = fonts[i].getName();
+        }
+
+        return names;
+    }
+
+    public String[] getSystemFontsPaths() {
+        String[] result;
+        if (SystemUtils.IS_OS_WINDOWS) {
+            result = new String[1];
+            String path = System.getenv("WINDIR");
+            result[0] = path + "\\" + "Fonts";
+            return result;
+        } else if(SystemUtils.IS_OS_MAC_OSX || SystemUtils.IS_OS_MAC) {
+            result = new String[4];
+            result[0] = "~/Library/Fonts";
+            result[1] = "/Library/Fonts";
+            result[2] = "/System/Library/Fonts";
+            return result;
+        } else if (SystemUtils.IS_OS_LINUX) {
+            result = new String[1];
+            result[0] = "~/.fonts";
+            return result;
+        }
+
+        return null;
+    }
+
+    public List<File> getSystemFontFiles() {
+        // only retrieving ttf files
+        String[] extensions = new String[] { "ttf" };
+        String[] paths = getSystemFontsPaths();
+
+        ArrayList<File> files = new ArrayList<>();
+
+        for(int i = 0; i < paths.length; i++) {
+            File fontDirectory = new File(paths[i]);
+            files.addAll(FileUtils.listFiles(fontDirectory, extensions, true));
+        }
+
+        return files;
+    }
+
+    public void preCacheSystemFontsMap() {
+        List<File> fontFiles = getSystemFontFiles();
+
+        for (File file : fontFiles) {
+            Font f = null;
+            try {
+                if(!systemFontMap.containsValue(file.getAbsolutePath())) {
+                    f = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(file.getAbsolutePath()));
+                    String name = f.getName();
+                    systemFontMap.put(name, file.getAbsolutePath());
+                }
+            } catch (FontFormatException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        prefs.put(systemFontMap);
+        prefs.flush();
+    }
+
+    public void loadCachedSystemFontMap() {
+        systemFontMap = (HashMap<String, String>)prefs.get();
+    }
+
+    public void invalidateFontMap() {
+        Array<String> names = new Array<>(getSystemFontNames());
+        for(Iterator<Map.Entry<String, String>> it = systemFontMap.entrySet().iterator(); it.hasNext(); ) {
+            Map.Entry<String, String> entry = it.next();
+            if(!names.contains(entry.getKey(), false)) {
+                it.remove();
+            }
+        }
+    }
+
+    public void generateFontsMap() {
+        loadCachedSystemFontMap();
+        preCacheSystemFontsMap();
+        //invalidateFontMap();
+    }
+
+    public HashMap<String, String> getFontsMap() {
+        if(systemFontMap.size() == 0) {
+            generateFontsMap();
+        }
+        return systemFontMap;
+    }
+
+    public Array<String> getFontNamesFromMap() {
+        Array<String> fontNames = new Array<>();
+        for (Map.Entry<String, String> entry : systemFontMap.entrySet()) {
+            fontNames.add(entry.getKey());
+        }
+
+        return fontNames;
+    }
+
+    public FileHandle getTTFByName(String fontName) {
+        return new FileHandle(systemFontMap.get(fontName));
+    }
+}
