e9a0af27d32f504500bc53822f3dcc358c2e767d
Gevorg Kopalyan <gevorg.kopalyan@gmail.com>
2015-05-15 18:24:07 +0400

    Global Input Adapter for all type of inputs

--- src/com/uwsoft/editor/mvc/view/stage/input/SandboxInputAdapter.java
+++ src/com/uwsoft/editor/mvc/view/stage/input/SandboxInputAdapter.java
@@ -1,14 +1,32 @@
 package com.uwsoft.editor.mvc.view.stage.input;
 
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.utils.Array;
 import com.uwsoft.editor.mvc.Overlap2DFacade;
+import com.uwsoft.editor.mvc.view.stage.SandboxMediator;
+import com.uwsoft.editor.renderer.conponents.light.LightObjectComponent;
 
 public class SandboxInputAdapter implements InputProcessor {
 
 	private Overlap2DFacade facade;
+	private Family family;
+	private Engine engine;
+	private ImmutableArray<Entity> entities;
+	private ComponentMapper<InputListenerComponent> inputListenerCM; 
+	private InputListenerComponent inpputListenerComponent;
+	private Entity target;
 
 	public SandboxInputAdapter() {
 		facade = Overlap2DFacade.getInstance();
+		SandboxMediator sandboxMediator = facade.retrieveMediator(SandboxMediator.NAME);
+		engine = sandboxMediator.getViewComponent().getEngine();
+		family = Family.all(InputListenerComponent.class).get();
+		inputListenerCM = ComponentMapper.getFor(InputListenerComponent.class);
 	}
 
 	@Override
@@ -31,31 +49,80 @@ public class SandboxInputAdapter implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
+		entities = engine.getEntitiesFor(family);
+		for (int i = 0, n = entities.size(); i < n; i++){
+			Entity entity = entities.get(i);
+			inpputListenerComponent = inputListenerCM.get(entity);
+			Array<InputListener> asd = inpputListenerComponent.getAllListeners();
+			for (int j = 0, s = asd.size; j < s; j++){
+				if (asd.get(j).touchDown(entity, screenX, screenY, pointer, button)){
+					target = entity;
+					return true;
+				}
+			}
+			
+		}
 		return false;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
+		if(target == null){
+			return false;
+		}
+		inpputListenerComponent = inputListenerCM.get(target);
+		Array<InputListener> asd = inpputListenerComponent.getAllListeners();
+		for (int j = 0, s = asd.size; j < s; j++){
+			asd.get(j).touchUp(target, screenX, screenY, pointer, button);
+		}
+		target = null;
+		return true;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
+		if(target == null){
+			return false;
+		}
+		inpputListenerComponent = inputListenerCM.get(target);
+		Array<InputListener> asd = inpputListenerComponent.getAllListeners();
+		for (int j = 0, s = asd.size; j < s; j++){
+			asd.get(j).touchDragged(target, screenX, screenY, pointer);
+		}
+		return true;
 	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
+		entities = engine.getEntitiesFor(family);
+		for (int i = 0, n = entities.size(); i < n; i++){
+			Entity entity = entities.get(i);
+			inpputListenerComponent = inputListenerCM.get(entity);
+			Array<InputListener> asd = inpputListenerComponent.getAllListeners();
+			for (int j = 0, s = asd.size; j < s; j++){
+				if (asd.get(j).mouseMoved(entity, screenX, screenY)){
+					return true;
+				}
+			}
+			
+		}
 		return false;
 	}
 
 	@Override
 	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
+		entities = engine.getEntitiesFor(family);
+		for (int i = 0, n = entities.size(); i < n; i++){
+			Entity entity = entities.get(i);
+			inpputListenerComponent = inputListenerCM.get(entity);
+			Array<InputListener> asd = inpputListenerComponent.getAllListeners();
+			for (int j = 0, s = asd.size; j < s; j++){
+				if (asd.get(j).scrolled(entity,amount)){
+					return true;
+				}
+			}
+			
+		}
 		return false;
 	}
 
