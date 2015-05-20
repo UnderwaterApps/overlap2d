e27f89824d39f4f1a0584fe4e953336f3040f217
azakhary <avetis.zakharyan@gmail.com>
2015-05-20 17:53:44 +0400

    followers logic

--- src/com/uwsoft/editor/mvc/view/MidUIMediator.java
+++ src/com/uwsoft/editor/mvc/view/MidUIMediator.java
@@ -18,9 +18,18 @@
 
 package com.uwsoft.editor.mvc.view;
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.utils.ImmutableArray;
 import com.puremvc.patterns.mediator.SimpleMediator;
 import com.puremvc.patterns.observer.Notification;
 import com.uwsoft.editor.Overlap2D;
+import com.uwsoft.editor.gdx.sandbox.Sandbox;
+import com.uwsoft.editor.mvc.proxy.SceneDataManager;
+import com.uwsoft.editor.mvc.view.ui.followers.BasicFollower;
+import com.uwsoft.editor.mvc.view.ui.followers.LightFollower;
+import com.uwsoft.editor.mvc.view.ui.followers.SelectionFollower;
+
+import java.util.HashMap;
 
 /**
  * Created by azakhary on 5/20/2015.
@@ -28,6 +37,8 @@ import com.uwsoft.editor.Overlap2D;
 public class MidUIMediator extends SimpleMediator<MidUI> {
     private static final String TAG = MidUIMediator.class.getCanonicalName();
     public static final String NAME = TAG;
+
+    private HashMap<Entity, BasicFollower> followers = new HashMap<>();
 
     public MidUIMediator() {
         super(NAME, new MidUI());
@@ -41,6 +52,7 @@ public class MidUIMediator extends SimpleMediator<MidUI> {
     @Override
     public String[] listNotificationInterests() {
         return new String[]{
+                SceneDataManager.SCENE_LOADED,
                 Overlap2D.ITEM_DATA_UPDATED
         };
     }
@@ -49,8 +61,40 @@ public class MidUIMediator extends SimpleMediator<MidUI> {
     public void handleNotification(Notification notification) {
         super.handleNotification(notification);
         switch (notification.getName()) {
+            case SceneDataManager.SCENE_LOADED:
+                //createFollowersForAllItems();
+                break;
             case Overlap2D.ITEM_DATA_UPDATED:
+                BasicFollower follower = followers.get(notification.getBody());
+                if(follower != null) {
+                    follower.update();
+                }
                 break;
         }
+    }
+
+    private void createFollowersForAllItems() {
+        Sandbox sandbox = Sandbox.getInstance();
+        ImmutableArray<Entity> entities = sandbox.getEngine().getEntities();
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
+            createFollower(entity);
+        }
+    }
+
+    private void removeAllfollowers() {
+        followers.values().forEach(com.uwsoft.editor.mvc.view.ui.followers.BasicFollower::remove);
+        followers.clear();
+    }
+
+    private void createFollower(Entity entity) {
+        BasicFollower follower = new SelectionFollower(entity);
+        viewComponent.addActor(follower);
+        followers.put(entity, follower);
+    }
+
+    private void removeFollower(Entity entity) {
+        followers.get(entity).remove();
+        followers.remove(entity);
     }
 }
