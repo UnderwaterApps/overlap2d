fccd3d6d00767bba273aa2c7776f6ef50e64da81
Gevorg Kopalyan <gevorg.kopalyan@gmail.com>
2015-06-04 20:55:13 +0400

    renderer fix for composites

--- src/com/uwsoft/editor/renderer/components/NodeComponent.java
+++ src/com/uwsoft/editor/renderer/components/NodeComponent.java
@@ -1,8 +1,9 @@
 package com.uwsoft.editor.renderer.components;
 
+import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.utils.SnapshotArray;
 
-public class NodeComponent extends ParentNodeComponent {
+public class NodeComponent extends Component {
 	public SnapshotArray<Entity> children = new SnapshotArray<Entity>(true, 1, Entity.class);
 }
