ee2d72b4fe1083991b4b925a62314ac4749cf520
azakhary <avetis.zakharyan@gmail.com>
2015-06-15 21:18:47 +0400

    label resize

--- src/com/uwsoft/editor/mvc/view/ui/followers/LabelFollower.java
+++ src/com/uwsoft/editor/mvc/view/ui/followers/LabelFollower.java
@@ -19,17 +19,75 @@
 package com.uwsoft.editor.mvc.view.ui.followers;
 
 import com.badlogic.ashley.core.Entity;
+
+import java.util.HashMap;
+
 /**
  * Created by azakhary on 5/20/2015.
  */
-public class LabelFollower extends NormalSelectionFollower {
+public class LabelFollower extends NormalSelectionFollower implements FollowerTransformationListener{
+
+    HashMap<Integer, LabelAnchorListener> anchorListeners;
 
     public LabelFollower(Entity entity) {
         super(entity);
     }
 
     @Override
+    public void create() {
+        super.create();
+        anchorListeners = new HashMap<>();
+        for(int i = 0; i < miniRects.length; i++) {
+            LabelAnchorListener listener = new LabelAnchorListener(this, this, i);
+            anchorListeners.put(i, listener);
+            miniRects[i].addListener(listener);
+        }
+        transformGroup.setVisible(true);
+        miniRects[ORIGIN].setVisible(false);
+    }
+
+    @Override
     public void update() {
         super.update();
+    }
+
+    @Override
+    public void setFollowerListener(FollowerTransformationListener listener) {
+        for(int i = 0; i < miniRects.length; i++) {
+            anchorListeners.get(i).setListenerTransform(listener);
+        }
+    }
+    public void setMode(SelectionMode mode) {
+        super.setMode(mode);
+        transformGroup.setVisible(true);
+        miniRects[ORIGIN].setVisible(false);
+        if(mode == SelectionMode.transform) {
+            miniRects[ORIGIN].setVisible(true);
+        }
+    }
+
+    @Override
+    public void anchorDown(NormalSelectionFollower follower, int anchor, float x, float y) {
+
+    }
+
+    @Override
+    public void anchorDragged(NormalSelectionFollower follower, int anchor, float x, float y) {
+        update();
+    }
+
+    @Override
+    public void anchorUp(NormalSelectionFollower follower, int anchor, float x, float y) {
+
+    }
+
+    @Override
+    public void anchorMouseEnter(NormalSelectionFollower follower, int anchor, float x, float y) {
+
+    }
+
+    @Override
+    public void anchorMouseExit(NormalSelectionFollower follower, int anchor, float x, float y) {
+
     }
 }
