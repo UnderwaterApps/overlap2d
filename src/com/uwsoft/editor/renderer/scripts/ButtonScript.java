51b9ec44a2137195b624d57f9e135df4e134dec8
azakhary <avetis.zakharyan@gmail.com>
2015-06-22 13:55:33 +0400

    refactorings

--- src/com/uwsoft/editor/renderer/scripts/ButtonScript.java
+++ src/com/uwsoft/editor/renderer/scripts/ButtonScript.java
@@ -1,5 +1,11 @@
 package com.uwsoft.editor.renderer.scripts;
 
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.uwsoft.editor.renderer.components.LayerMapComponent;
+
 /**
  * Created by azakhary on 6/19/2015.
  */
@@ -9,9 +15,45 @@ public class ButtonScript extends BasicScript {
     public static final String overLayer = "over";
     public static final String pressedLayer = "pressed";
 
+    boolean isChecked, isDisabled;
+    boolean isOver, isVisualPressed;
+
+    private ComponentMapper<LayerMapComponent> layerMapComponentComponentMapper;
+
+    public ButtonScript() {
+        layerMapComponentComponentMapper = ComponentMapper.getFor(LayerMapComponent.class);
+    }
+
     @Override
     public void act(float delta) {
+        LayerMapComponent layerMapComponent = layerMapComponentComponentMapper.get(entity);
+        if(isPressed()) {
+            //layerMapComponent.layers.get()
+        }
+    }
 
+    public void setChecked (boolean isChecked) {
+        this.isChecked = isChecked;
+    }
+
+    public void toggle () {
+        setChecked(!isChecked);
+    }
+
+    public boolean isChecked () {
+        return isChecked;
+    }
+
+    public boolean isPressed () {
+        return isVisualPressed;
+    }
+
+    public boolean isOver () {
+        return isOver;
+    }
+
+    public boolean isDisabled () {
+        return isDisabled;
     }
 
     @Override
