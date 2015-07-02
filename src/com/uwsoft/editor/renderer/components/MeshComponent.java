2cf2d428cf3a467327266163bfde1df26af33122
azakhary <avetis.zakharyan@gmail.com>
2015-07-02 22:53:03 +0400

    vertices meshes

--- src/com/uwsoft/editor/renderer/components/MeshComponent.java
+++ src/com/uwsoft/editor/renderer/components/MeshComponent.java
@@ -19,6 +19,7 @@
 package com.uwsoft.editor.renderer.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -27,7 +28,14 @@ import com.badlogic.gdx.math.Vector2;
 public class MeshComponent extends Component {
     public Vector2[][] vertices;
 
-    public void addVertice(Vector2 pos) {
+    public void makeRectangle(float width, float height) {
+        Vector2[] points = new Vector2[4];
+        points[0] = new Vector2(0, 0);
+        points[1] = new Vector2(0, height);
+        points[2] = new Vector2(width, height);
+        points[3] = new Vector2(width, 0);
 
+        vertices = new Vector2[1][4];
+        vertices[0] = points;
     }
 }
