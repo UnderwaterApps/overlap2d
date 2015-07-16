24ee09622946bc0aafe7c49ec4ce7c6357258a34
azakhary <avetis.zakharyan@gmail.com>
2015-07-16 15:09:52 +0400

    9patch madness

--- src/com/uwsoft/editor/renderer/factory/component/NinePatchComponentFactory.java
+++ src/com/uwsoft/editor/renderer/factory/component/NinePatchComponentFactory.java
@@ -11,6 +11,8 @@ import com.uwsoft.editor.renderer.components.DimensionsComponent;
 import com.uwsoft.editor.renderer.components.NinePatchComponent;
 import com.uwsoft.editor.renderer.data.Image9patchVO;
 import com.uwsoft.editor.renderer.data.MainItemVO;
+import com.uwsoft.editor.renderer.data.ProjectInfoVO;
+import com.uwsoft.editor.renderer.data.ResolutionEntryVO;
 import com.uwsoft.editor.renderer.factory.EntityFactory;
 import com.uwsoft.editor.renderer.resources.IResourceRetriever;
 
@@ -36,7 +38,6 @@ public class NinePatchComponentFactory extends ComponentFactory {
 		DimensionsComponent component = new DimensionsComponent();
 		component.height = ((Image9patchVO) vo).height;
 		component.width = ((Image9patchVO) vo).width;
-
 		if(component.width == 0) {
 			component.width = ninePatchComponent.ninePatch.getTotalWidth();
 		}
@@ -53,6 +54,13 @@ public class NinePatchComponentFactory extends ComponentFactory {
 		NinePatchComponent ninePatchComponent = new NinePatchComponent();
 		AtlasRegion atlasRegion = (TextureAtlas.AtlasRegion) rm.getTextureRegion(vo.imageName);
 		ninePatchComponent.ninePatch = new NinePatch(atlasRegion, atlasRegion.splits[0], atlasRegion.splits[1], atlasRegion.splits[2], atlasRegion.splits[3]);
+
+		ProjectInfoVO projectInfoVO = rm.getProjectVO();
+
+		ninePatchComponent.ninePatch.scale(1f/projectInfoVO.pixelToWorld, 1f/projectInfoVO.pixelToWorld);
+		ninePatchComponent.ninePatch.setMiddleWidth(ninePatchComponent.ninePatch.getMiddleWidth()/projectInfoVO.pixelToWorld);
+		ninePatchComponent.ninePatch.setMiddleHeight(ninePatchComponent.ninePatch.getMiddleHeight()/projectInfoVO.pixelToWorld);
+
 		ninePatchComponent.textureRegionName = vo.imageName;
 		entity.add(ninePatchComponent);
 
