abbb6368dcb9b9f4b34131c8638380a256efb28d
azakhary <avetis.zakharyan@gmail.com>
2015-07-20 12:46:43 +0400

    removed some imports

--- src/com/uwsoft/editor/renderer/components/SpineDataComponent.java
+++ src/com/uwsoft/editor/renderer/components/SpineDataComponent.java
@@ -1,16 +1,6 @@
 package com.uwsoft.editor.renderer.components;
 
 import com.badlogic.ashley.core.Component;
-import com.badlogic.gdx.utils.Array;
-import com.esotericsoftware.spine.Animation;
-import com.esotericsoftware.spine.AnimationState;
-import com.esotericsoftware.spine.Skeleton;
-import com.esotericsoftware.spine.SkeletonData;
-import com.esotericsoftware.spine.SkeletonJson;
-import com.esotericsoftware.spine.Slot;
-import com.esotericsoftware.spine.attachments.Attachment;
-import com.esotericsoftware.spine.attachments.RegionAttachment;
-import com.uwsoft.editor.renderer.components.DimensionsComponent;
 
 public class SpineDataComponent extends Component {
 	public String animationName = "";
