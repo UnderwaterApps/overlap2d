e3d5210f07f8ec8a6ec544f4a3b8e581a0be64fb
azakhary <avetis.zakharyan@gmail.com>
2015-07-26 20:16:59 +0400

    made empty class

--- src/com/uwsoft/editor/controller/commands/ReOrderLayerCommand.java
+++ src/com/uwsoft/editor/controller/commands/ReOrderLayerCommand.java
@@ -3,5 +3,14 @@ package com.uwsoft.editor.controller.commands;
 /**
  * Created by CyberJoe on 7/25/2015.
  */
-public class ReOrderLayerCommand {
+public class ReOrderLayerCommand extends EntityModifyRevertableCommand {
+    @Override
+    public void doAction() {
+
+    }
+
+    @Override
+    public void undoAction() {
+
+    }
 }
