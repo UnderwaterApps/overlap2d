42b50ba9e2c0eea94e56cc4eadc6aff29c93ced9
Osman <lmgtsoft@gmail.com>
2015-08-21 01:54:36 +0200

    - CompositeActor's build process now customizable (enables extensions for tag handling for example)

--- src/com/uwsoft/editor/renderer/scene2d/CompositeActor.java
+++ src/com/uwsoft/editor/renderer/scene2d/CompositeActor.java
@@ -32,12 +32,20 @@ public class CompositeActor extends Group {
     private HashMap<String, LayerItemVO> layerMap = new HashMap<String, LayerItemVO>();
 
     public CompositeActor(CompositeItemVO vo, IResourceRetriever ir) {
+        this(vo, ir, BuiltItemHandler.DEFAULT);
+    }
+
+    public CompositeActor(CompositeItemVO vo, IResourceRetriever ir, BuiltItemHandler itemHandler) {
+        this(vo, ir, itemHandler, true);
+    }
+
+    private CompositeActor(CompositeItemVO vo, IResourceRetriever ir, BuiltItemHandler itemHandler, boolean isRoot) {
         this.ir= ir;
         this.vo = vo;
 
         pixelsPerWU = ir.getProjectVO().pixelToWorld;
         makeLayerMap(vo);
-        build(vo);
+        build(vo, itemHandler, isRoot);
     }
 
     private void makeLayerMap(CompositeItemVO vo) {
@@ -47,60 +55,90 @@ public class CompositeActor extends Group {
         }
     }
 
-    private void build(CompositeItemVO vo) {
-        buildImages(vo.composite.sImages);
-        build9PatchImages(vo.composite.sImage9patchs);
-        buildLabels(vo.composite.sLabels);
-        buildComposites(vo.composite.sComposites);
+    private void build(CompositeItemVO vo, BuiltItemHandler itemHandler, boolean isRoot) {
+        buildImages(vo.composite.sImages, itemHandler);
+        build9PatchImages(vo.composite.sImage9patchs, itemHandler);
+        buildLabels(vo.composite.sLabels, itemHandler);
+        buildComposites(vo.composite.sComposites, itemHandler);
         processZIndexes();
         recalculateSize();
 
-        if(vo.tags != null && Arrays.asList(vo.tags).contains("button")) {
-            addListener(new ButtonClickListener());
+        if(isRoot) {
+            buildCoreData(this, vo);
+            itemHandler.onItemBuild(this);
         }
     }
 
-    private void buildComposites(ArrayList<CompositeItemVO> composites) {
+    private void buildComposites(ArrayList<CompositeItemVO> composites, BuiltItemHandler itemHandler) {
         for(int i = 0; i < composites.size(); i++) {
-            CompositeActor actor = new CompositeActor(composites.get(i), ir);
+            CompositeActor actor = new CompositeActor(composites.get(i), ir, itemHandler, false);
             processMain(actor, composites.get(i));
             addActor(actor);
+
+            itemHandler.onItemBuild(actor);
         }
     }
 
-    private void buildImages(ArrayList<SimpleImageVO> images) {
+    private void buildImages(ArrayList<SimpleImageVO> images, BuiltItemHandler itemHandler) {
         for(int i = 0; i < images.size(); i++) {
             Image image = new Image(ir.getTextureRegion(images.get(i).imageName));
             processMain(image, images.get(i));
             addActor(image);
+
+            itemHandler.onItemBuild(image);
         }
     }
 
-    private void build9PatchImages(ArrayList<Image9patchVO> patches) {
+    private void build9PatchImages(ArrayList<Image9patchVO> patches, BuiltItemHandler itemHandler) {
         for(int i = 0; i < patches.size(); i++) {
             TextureAtlas.AtlasRegion region = (TextureAtlas.AtlasRegion) ir.getTextureRegion(patches.get(i).imageName);
             NinePatch ninePatch = new NinePatch(region, region.splits[0], region.splits[1], region.splits[2], region.splits[3]);
             Image image = new Image(ninePatch);
             image.setWidth(patches.get(i).width*pixelsPerWU);
-            image.setHeight(patches.get(i).height*pixelsPerWU);
+            image.setHeight(patches.get(i).height * pixelsPerWU);
             processMain(image, patches.get(i));
             addActor(image);
+
+            itemHandler.onItemBuild(image);
         }
     }
 
-    private void buildLabels(ArrayList<LabelVO> labels) {
+    private void buildLabels(ArrayList<LabelVO> labels, BuiltItemHandler itemHandler) {
         for(int i = 0; i < labels.size(); i++) {
             Label.LabelStyle style = new Label.LabelStyle(ir.getBitmapFont(labels.get(i).style, labels.get(i).size), Color.WHITE);
             Label label = new Label(labels.get(i).text, style);
             label.setAlignment(labels.get(i).align);
-            label.setWidth(labels.get(i).width*pixelsPerWU);
-            label.setHeight(labels.get(i).height*pixelsPerWU);
+            label.setWidth(labels.get(i).width * pixelsPerWU);
+            label.setHeight(labels.get(i).height * pixelsPerWU);
             processMain(label, labels.get(i));
             addActor(label);
+
+            itemHandler.onItemBuild(label);
         }
     }
 
     private void processMain(Actor actor, MainItemVO vo) {
+
+        buildCoreData(actor, vo);
+
+        //actor properties
+        actor.setPosition(vo.x * pixelsPerWU, vo.y * pixelsPerWU);
+        actor.setOrigin(vo.originX * pixelsPerWU, vo.originY * pixelsPerWU);
+        actor.setScale(vo.scaleX, vo.scaleY);
+        actor.setRotation(vo.rotation);
+        actor.setColor(new Color(vo.tint[0], vo.tint[1], vo.tint[2], vo.tint[3]));
+
+        indexes.put(getLayerIndex(vo.layerName) + vo.zIndex, actor);
+
+        if(layerMap.get(vo.layerName).isVisible) {
+            actor.setVisible(true);
+        } else {
+            actor.setVisible(false);
+        }
+    }
+
+    private void buildCoreData(Actor actor, MainItemVO vo){
+
         //custom variables
         CustomVariables cv = null;
         if(vo.customVars != null && !vo.customVars.isEmpty()) {
@@ -115,21 +153,7 @@ public class CompositeActor extends Group {
         data.tags = vo.tags;
         data.customVars = cv;
 
-        //actor properties
-        actor.setPosition(vo.x * pixelsPerWU, vo.y * pixelsPerWU);
-        actor.setOrigin(vo.originX * pixelsPerWU, vo.originY * pixelsPerWU);
-        actor.setScale(vo.scaleX, vo.scaleY);
-        actor.setRotation(vo.rotation);
-        actor.setColor(new Color(vo.tint[0], vo.tint[1], vo.tint[2], vo.tint[3]));
         actor.setUserObject(data);
-
-        indexes.put(data.layerIndex + vo.zIndex, actor);
-
-        if(layerMap.get(vo.layerName).isVisible) {
-            actor.setVisible(true);
-        } else {
-            actor.setVisible(false);
-        }
     }
 
 
@@ -258,5 +282,29 @@ public class CompositeActor extends Group {
 
     public CompositeItemVO getVo() {
         return vo;
+    }
+
+    /**
+     * Enables customization of the CompositeActor during building.
+     * Example use cases: tag handling, custom variables handling
+     */
+    public interface BuiltItemHandler {
+
+        /**
+         * @param item newly built and added to a parent (in case it's not a root actor)
+         */
+        void onItemBuild(Actor item);
+
+        BuiltItemHandler DEFAULT = new BuiltItemHandler() {
+            @Override
+            public void onItemBuild(Actor item) {
+
+                if(item instanceof CompositeActor) {
+                    CoreActorData data = (CoreActorData) item.getUserObject();
+                    if(data != null && data.tags != null && Arrays.asList(data.tags).contains("button"))
+                        item.addListener(new ButtonClickListener());
+                }
+            }
+        };
     }
 }
