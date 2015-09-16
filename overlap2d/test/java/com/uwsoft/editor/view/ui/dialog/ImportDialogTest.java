40b28dff8987571043f51b0bf6ed3157f4846b16
Huang YunKun <htynkn@gmail.com>
2015-09-16 15:29:19 +0000

    Add more ui test

---
+++ overlap2d/test/java/com/uwsoft/editor/view/ui/dialog/ImportDialogTest.java
@@ -0,0 +1,36 @@
+package com.uwsoft.editor.view.ui.dialog;
+
+import com.badlogic.gdx.Gdx;
+import com.kotcrab.vis.ui.VisUI;
+import com.runner.LibgdxRunner;
+import com.runner.NeedGL;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.internal.util.reflection.Whitebox;
+
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(LibgdxRunner.class)
+public class ImportDialogTest {
+    private ImportDialog importDialog;
+
+    @Before
+    public void setUp() throws Exception {
+        VisUI.load(Gdx.files.local("overlap2d/assets/style/uiskin.json"));
+        importDialog = new ImportDialog();
+    }
+
+    @Test
+    @NeedGL
+    public void shouldFillTypeNames() throws Exception {
+        HashMap typeNames = (HashMap) Whitebox.getInternalState(importDialog, "typeNames");
+
+        assertThat(typeNames.size(), is(9));
+        assertThat(typeNames.get(1), is("Texture"));
+        assertThat(typeNames.get(9), is("Texture Atlas"));
+    }
+}
