1345a8a270bbc183d2153d8ef9772cba72ec0867
Huang YunKun <htynkn@gmail.com>
2015-09-16 12:41:44 +0000

    Limit gdx instance, share one for all testes

--- overlap2d/test/java/com/runner/LibgdxRunner.java
+++ overlap2d/test/java/com/runner/LibgdxRunner.java
@@ -8,8 +8,6 @@ import com.runner.exception.LibgdxInitException;
 import com.runner.util.ConditionWaiter;
 import org.apache.commons.io.FileUtils;
 import org.junit.runner.Description;
-import org.junit.runner.Result;
-import org.junit.runner.notification.RunListener;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
@@ -21,12 +19,15 @@ import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class LibgdxRunner extends BlockJUnit4ClassRunner {
-    private File prefs;
     private Random random = new Random();
+    private static File prefs;
+    private static AtomicBoolean init = new AtomicBoolean(false);
 
     public LibgdxRunner(Class<?> klass) throws InitializationError {
         super(klass);
-        initApplication();
+        if (init.compareAndSet(false, true)) {
+            initApplication();
+        }
     }
 
     private void initApplication() {
@@ -40,13 +41,16 @@ public class LibgdxRunner extends BlockJUnit4ClassRunner {
             new JglfwApplication(new TestApplicationListener(), cfg);
             ConditionWaiter.wait(() -> Gdx.files != null, "Jglfw init failed.", 10);
             prefs = new File(Gdx.files.getExternalStoragePath(), "tmp/");
-            safeCleanDir();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                safeCleanDir();
+                closeGdxApplication();
+            }));
         } catch (Exception ex) {
             throw new LibgdxInitException(ex);
         }
     }
 
-    public void safeCleanDir() {
+    private void safeCleanDir() {
         try {
             FileUtils.deleteDirectory(prefs);
         } catch (IOException e) {
@@ -54,9 +58,12 @@ public class LibgdxRunner extends BlockJUnit4ClassRunner {
         }
     }
 
+    private void closeGdxApplication() {
+        Gdx.app.exit();
+    }
+
     @Override
     public void run(RunNotifier notifier) {
-        notifier.addListener(new CloseLibgdxListener());
         super.run(notifier);
     }
 
@@ -73,7 +80,9 @@ public class LibgdxRunner extends BlockJUnit4ClassRunner {
                 }
                 running.set(false);
             });
-            ConditionWaiter.wait(() -> !running.get(), description, 30);
+            ConditionWaiter.wait(() -> !running.get(), description, 30, () -> {
+                closeGdxApplication();
+            });
         } else {
             runLeaf(methodBlock(method), description, notifier);
         }
@@ -81,13 +90,5 @@ public class LibgdxRunner extends BlockJUnit4ClassRunner {
 
     private class TestApplicationListener extends ApplicationAdapter {
 
-    }
-
-    private class CloseLibgdxListener extends RunListener {
-        @Override
-        public void testRunFinished(Result result) throws Exception {
-            Gdx.app.exit();
-            ConditionWaiter.wait(() -> Gdx.app == null, "Gdx exit failed", 20);
-        }
     }
 }
