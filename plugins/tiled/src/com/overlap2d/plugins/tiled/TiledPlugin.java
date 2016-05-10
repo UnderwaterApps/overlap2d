e2911f65eb625b7414fd9c29a31a600414ddfd9f
mariamKh <mariam@underwaterapps.com>
2016-05-10 20:09:50 +0400

    initial

--- plugins/tiled/src/com/overlap2d/plugins/tiled/TiledPlugin.java
+++ plugins/tiled/src/com/overlap2d/plugins/tiled/TiledPlugin.java
@@ -67,10 +67,6 @@ public class TiledPlugin extends O2DPluginAdapter {
     public TiledPanelMediator tiledPanelMediator;
     public ResourcesManager pluginRM;
 
-    public TransformComponent leftComponent;
-    public TransformComponent bottomComponent;
-    private Entity leftEntity, bottomEntity;
-
     public TiledPlugin() {
         tiledPanelMediator = new TiledPanelMediator(this);
     }
@@ -144,22 +140,5 @@ public class TiledPlugin extends O2DPluginAdapter {
     public boolean isOnCurrentSelectedLayer(Entity entity) {
         ZIndexComponent entityZComponent = ComponentRetriever.get(entity, ZIndexComponent.class);
         return entityZComponent.layerName.equals(pluginAPI.getCurrentSelectedLayerName());
-    }
-
-    public void initPluginMinEntities() {
-        bottomEntity = leftEntity = pluginAPI.getProjectEntities().iterator().next();
-        pluginAPI.getProjectEntities().forEach(entity -> {
-            TransformComponent transformComponent = ComponentRetriever.get(entity, TransformComponent.class);
-            leftComponent = ComponentRetriever.get(leftEntity, TransformComponent.class);
-            bottomComponent = ComponentRetriever.get(bottomEntity, TransformComponent.class);
-            if (transformComponent.x < leftComponent.x) {
-                leftComponent = transformComponent;
-                leftEntity = entity;
-            }
-            if (transformComponent.y < bottomComponent.y) {
-                bottomComponent = transformComponent;
-                bottomEntity = entity;
-            }
-        });
     }
 }
